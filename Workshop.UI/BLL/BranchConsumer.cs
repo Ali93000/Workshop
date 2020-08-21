5e3f476b54d3c8547ed7fd116c48dd4a6a2e56c2
Ali Mostafa <wekaa176@outlook.com>
2020-08-21 21:08:29 +0200

    .

--- Workshop.UI/BLL/BranchConsumer.cs
+++ Workshop.UI/BLL/BranchConsumer.cs
@@ -24,7 +24,30 @@ namespace Workshop.UI.BLL
             {
                 client.BaseAddress = new Uri(ApiURL);
                 ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
-                var responseTask = client.GetAsync("branchs?compId="+ compId).Result;
+                var responseTask = client.GetAsync("branchs?compId=" + compId).Result;
+                if (responseTask.IsSuccessStatusCode)
+                {
+                    branchesResponse = responseTask.Content.ReadAsAsync<BranchesResponse>().Result;
+                    return branchesResponse;
+                }
+                else
+                {
+                    return new BranchesResponse();
+                }
+            }
+        }
+
+        public BranchesResponse GetAllBranches()
+        {
+            BranchesResponse branchesResponse = new BranchesResponse();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ApiURL);
+                var token = HttpContext.Current.Session["token"] as string;
+
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+
+                var responseTask = client.GetAsync("branch").Result;
                 if (responseTask.IsSuccessStatusCode)
                 {
                     branchesResponse = responseTask.Content.ReadAsAsync<BranchesResponse>().Result;
